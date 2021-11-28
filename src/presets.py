cfe5e8b724bbc90a9415243c221167df13accc5c
AaronTheNerd <bargeaaron@gmail.com>
2021-11-28 11:00:07 -0500

    Added simple preset classes

---
+++ src/presets.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+
+import color
+from animation import Animation
+
+
+class Preset(ABC):
+    HOLD_MAX = (1 << 16) - 1
+    @abstractmethod
+    def get_animation(self):
+        pass
+
+
+class SolidColorPreset(Preset):
+    def __init__(self, color=color.WHITE):
+        self.color = color
+    def get_animation(self):
+        return Animation([{ i : self.color for i in range(50) }], Preset.HOLD_MAX)
+
+
+class SolidTwoColorPreset(Preset):
+    def __init__(self, color1=color.WHITE, color2=color.BLACK):
+        self.color1 = color1
+        self.color2 = color2
+    def get_animation(self):
+        return Animation([{i : self.color1 if i % 2 == 0 else self.color2 for i in range(50)}],
+                Preset.HOLD_MAX)
+
+class TwoColorAlternatingPreset(Preset):
+    def __init__(self, color1=color.WHITE, color2=color.BLACK, hold_ms=0):
+        self.color1 = color1
+        self.color2 = color2
+        self.hold_ms = hold_ms
+    def get_animation(self):
+        return Animation([ { i : self.color1 if i % 2 == j else self.color2 for i in range(50) }
+                for j in range(2) ],
+                self.hold_ms)
+
+                
